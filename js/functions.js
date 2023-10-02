06c6f8894ed95a45ed723e5a4da666c30dee315e
алексей <79536094118@yandex.ru>
2023-10-02 21:44:15 +0500

    исправления для второго задания

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,11 @@
-function checkStringLength(str, maxLength) {
+//Функция 1
+const checkStringLength = function(str, maxLength) {
   return str.length <= maxLength;
-}
+};
+checkStringLength('проверяемая строка', 20);
 
-function isPalindrome(str) {
+//Фунция 2
+const isPalindrome = function(str) {
   str = str.replaceAll(' ', '').toLowerCase();
   for (let i = 0; i < Math.floor(str.length / 2); i++) {
     if (str[i] !== str[str.length - 1 - i]) {
@@ -10,5 +13,20 @@ function isPalindrome(str) {
     }
   }
   return true;
-}
+};
+isPalindrome('топот');
 
+//Функция 3
+const getNumber = function(str){
+  str = str.toString();
+  let result = '';
+  for(let i = 0; i < str.length; i++){
+    str = str.toString.replaceAll(' ','');
+    if(!isNaN(str[i])){
+      const number = parseInt(str[i], 10);
+      result+=number;
+    }
+  }
+  return result==='' ? NaN : result;
+};
+getNumber('2023 год');
