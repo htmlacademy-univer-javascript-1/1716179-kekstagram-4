a87a9f52583293d0086e5674a587f3cf21c5da18
алексей <79536094118@yandex.ru>
2023-11-15 21:28:22 +0500

    Попытка исправить

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
 import { mekePhotoArray } from './data.js';
-import { renderThumbnails } from './paint.js';
-//import {renderGallery} from './gallery.js';
-renderThumbnails(mekePhotoArray());
-//renderGallery();
+
+console.log(mekePhotoArray());
